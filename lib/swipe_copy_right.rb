830b42ee4d46db9b61ed1e0f2b24820a092092e3
danish rasheed <danishrashed@live.com>
2024-05-01 15:56:47 +0500

    changes in renderer

--- lib/swipe_copy_right.rb
+++ lib/swipe_copy_right.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "swipe_copy_right/version"
+require_relative "swipe_copy_right/renderer"
 
 module SwipeCopyRight
   class Error < StandardError; end
